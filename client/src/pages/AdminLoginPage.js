5ebf65440e683b3a99c22180f2fbf0e90331eb9d
Muhafud ali Farah <117611790+Dotmally@users.noreply.github.com>
2025-06-25 12:32:48 -0400

    removed axios

--- client/src/pages/AdminLoginPage.js
+++ client/src/pages/AdminLoginPage.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import './AdminLoginPage.css';
-import axios from 'axios';
 import api from '../api/config';
 
 function AdminLoginPage({ setIsAdmin }) {
